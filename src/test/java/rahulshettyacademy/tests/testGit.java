191646925b7ed40875d31efc4a1efd633d987452
Deoghan <deoghans24@gmail.com>
2024-05-17 13:50:46 +0200

    Initial files commit

---
+++ src/test/java/rahulshettyacademy/tests/testGit.java
@@ -0,0 +1,14 @@
+package rahulshettyacademy.tests;
+import org.testng.annotations.*;
+
+public class testGit {
+	
+@Test
+public void helloMsg()
+{
+	
+	System.out.println("This is a phase 2 change");
+	
+}
+
+}
